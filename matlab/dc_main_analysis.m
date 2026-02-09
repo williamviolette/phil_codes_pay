51083ac07ef669cf474785924617983cadda532b
williamviolette <43304544+williamviolette@users.noreply.github.com>
2026-02-09 09:06:06 -0500

    add moments

--- matlab/dc_main_analysis.m
+++ matlab/dc_main_analysis.m
@@ -5,7 +5,7 @@ cd "/Users/willviolette/Library/CloudStorage/Dropbox/Mac/Documents/GitHub/phil_c
 octave_setup;
 rng(1)
 
-folder ='/Users/willviolette/Library/CloudStorage/Dropbox/phil_temp_pay/moments/';
+folder ='/Users/willviolette/Library/CloudStorage/Dropbox/Mac/Documents/GitHub/phil_codes_pay/moments/';
 cd_dir ='/Users/willviolette/Library/CloudStorage/Dropbox/Mac/Documents/GitHub/phil_codes_pay/paper/tables_new/';
 
 
